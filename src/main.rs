8678d20a0ee3f10edb2c1beb2403c042bb2032de
xainglii-yao <Aarush.sharma.fpw@gmail.com>
2025-03-14 02:09:52 +0530

    added func to move cursor and type in raw_mode

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,63 @@
-mod store;
-use std::io;
-use store::{Node, write_file};
+use crossterm::{
+    cursor::MoveTo,
+    event, execute,
+    terminal::{Clear, ClearType, disable_raw_mode, enable_raw_mode},
+};
+use std::io::{Write, stdout};
 
 fn main() {
-    let mut input = String::new();
-    let stdin = io::stdin();
-    println!("enter data");
-    let _ = stdin.read_line(&mut input);
-    let res = write_file(input);
-    match res {
-        Ok(k) => println!("file created succesfully {:?}", k),
-        Err(e) => println!("nahh {}", e),
+    enable_raw_mode().unwrap();
+    let mut stdout = stdout();
+
+    let mut x = 0;
+    let mut y = 0;
+
+    execute!(stdout, Clear(ClearType::All)).unwrap();
+    loop {
+        execute!(stdout, MoveTo(x, y)).unwrap();
+        stdout.flush().unwrap();
+
+        if let event::Event::Key(key) = event::read().unwrap() {
+            match key.code {
+                event::KeyCode::Esc => break,
+                event::KeyCode::Up => {
+                    if y > 0 {
+                        y += 1;
+                    }
+                }
+                event::KeyCode::Down => {
+                    if y < 24 {
+                        y -= 1
+                    }
+                }
+                event::KeyCode::Left => {
+                    if x > 0 {
+                        x -= 1
+                    }
+                }
+                event::KeyCode::Right => {
+                    if x < 79 {
+                        x += 1
+                    }
+                }
+                event::KeyCode::Enter => {
+                    if y < 24 {
+                        y += 1;
+                    }
+                }
+                event::KeyCode::Backspace => {
+                    if x > 0 {
+                        x -= 1
+                    } else if y > 0 {
+                        y -= 1
+                    }
+                }
+                _ => {
+                    print!("{}", key.code);
+                    x += 1;
+                }
+            }
+        }
     }
-    let x = Node::default(25);
-    println!("{} {}", x.data, Node::square(&x));
+    disable_raw_mode().unwrap();
 }
