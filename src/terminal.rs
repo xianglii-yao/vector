f4fa7fbad17178deb13686a9fc68be4fb434d167
xainglii-yao <Aarush.sharma.fpw@gmail.com>
2025-03-24 02:26:30 +0530

    contains functions to manipulate terminal

---
+++ src/terminal.rs
@@ -0,0 +1,74 @@
+use crossterm::cursor::{Hide, MoveTo, Show};
+use crossterm::style::Print;
+use crossterm::terminal::{Clear, ClearType, disable_raw_mode, enable_raw_mode, size};
+use crossterm::{Command, queue};
+use std::io::{Error, Write, stdout};
+
+#[derive(Copy, Clone)]
+pub struct Size {
+    pub height: usize,
+    pub width: usize,
+}
+#[derive(Copy, Clone, Default)]
+pub struct Position {
+    pub col: usize,
+    pub row: usize,
+}
+
+pub struct Terminal;
+
+impl Terminal {
+    pub fn terminate() -> Result<(), Error> {
+        Self::execute()?;
+        disable_raw_mode()?;
+        Ok(())
+    }
+    pub fn initialize() -> Result<(), Error> {
+        enable_raw_mode()?;
+        Self::clear_screen()?;
+        Self::execute()?;
+        Ok(())
+    }
+    pub fn clear_screen() -> Result<(), Error> {
+        Self::queue_command(Clear(ClearType::All))?;
+        Ok(())
+    }
+    pub fn clear_line() -> Result<(), Error> {
+        Self::queue_command(Clear(ClearType::CurrentLine))?;
+        Ok(())
+    }
+    pub fn move_caret_to(position: Position) -> Result<(), Error> {
+        #[allow(clippy::as_conversions, clippy::cast_possible_truncation)]
+        Self::queue_command(MoveTo(position.col as u16, position.row as u16))?;
+        Ok(())
+    }
+    pub fn hide_caret() -> Result<(), Error> {
+        Self::queue_command(Hide)?;
+        Ok(())
+    }
+    pub fn show_caret() -> Result<(), Error> {
+        Self::queue_command(Show)?;
+        Ok(())
+    }
+    pub fn print(string: &str) -> Result<(), Error> {
+        Self::queue_command(Print(string))?;
+        Ok(())
+    }
+
+    pub fn size() -> Result<Size, Error> {
+        let (width_u16, height_u16) = size()?;
+        #[allow(clippy::as_conversions)]
+        let height = height_u16 as usize;
+        #[allow(clippy::as_conversions)]
+        let width = width_u16 as usize;
+        Ok(Size { height, width })
+    }
+    pub fn execute() -> Result<(), Error> {
+        stdout().flush()?;
+        Ok(())
+    }
+    fn queue_command<T: Command>(command: T) -> Result<(), Error> {
+        queue!(stdout(), command)?;
+        Ok(())
+    }
+}
