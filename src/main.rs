af7304a03afc64513ebf6be9a7827eb68206b699
xainglii-yao <Aarush.sharma.fpw@gmail.com>
2025-03-24 02:25:29 +0530

    moved code to corresponding file

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,9 @@
-use crossterm::{
-    cursor::MoveTo,
-    event, execute,
-    terminal::{Clear, ClearType, disable_raw_mode, enable_raw_mode},
-};
-use std::io::{Write, stdout};
-
+mod editor;
+use editor::Editor;
+mod buffer;
+mod terminal;
+mod view;
 fn main() {
-    enable_raw_mode().unwrap();
-    let mut stdout = stdout();
-
-    let mut x = 0;
-    let mut y = 0;
-
-    execute!(stdout, Clear(ClearType::All)).unwrap();
-    loop {
-        execute!(stdout, MoveTo(x, y)).unwrap();
-        stdout.flush().unwrap();
-
-        if let event::Event::Key(key) = event::read().unwrap() {
-            match key.code {
-                event::KeyCode::Esc => break,
-                event::KeyCode::Up => {
-                    if y > 0 {
-                        y += 1;
-                    }
-                }
-                event::KeyCode::Down => {
-                    if y < 24 {
-                        y -= 1
-                    }
-                }
-                event::KeyCode::Left => {
-                    if x > 0 {
-                        x -= 1
-                    }
-                }
-                event::KeyCode::Right => {
-                    if x < 79 {
-                        x += 1
-                    }
-                }
-                event::KeyCode::Enter => {
-                    if y < 24 {
-                        y += 1;
-                    }
-                }
-                event::KeyCode::Backspace => {
-                    if x > 0 {
-                        x -= 1
-                    } else if y > 0 {
-                        y -= 1
-                    }
-                }
-                _ => {
-                    print!("{}", key.code);
-                    x += 1;
-                }
-            }
-        }
-    }
-    disable_raw_mode().unwrap();
+    let mut editor = Editor::default();
+    Editor::run(&mut editor);
 }
